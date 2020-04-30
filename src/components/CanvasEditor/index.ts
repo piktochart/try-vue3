7de85a2580249b28302a20c73ec29716e240b536
chenxeed <chenxeed@gmail.com>
2020-04-30 17:37:49 +0800

    refactor(editor): convert to composition api

--- src/components/CanvasEditor/index.ts
+++ src/components/CanvasEditor/index.ts
@@ -1,98 +1,88 @@
-import { Vue, Options } from "vue-class-component";
-import imageUrl from "./test.jpg";
-import {
-  Block,
-  Blocks,
-  BlockList,
-  Item,
-  Items,
-  Coord,
-  ItemList
-} from "./types";
+import { defineComponent, reactive, toRefs } from "vue";
+import { Blocks, BlockList, Item, Items, Coord, ItemList } from "./types";
 
-@Options({})
-export default class CanvasEditor extends Vue {
-  setup() {
-    return;
-  }
-
-  private blocks: Blocks = {
-    "1": {
-      id: "1",
-      items: ["1", "2"]
-    }
-  };
-  private blockList: BlockList = ["1"];
-  private items: Items = {
-    "1": {
-      id: "1",
-      x: 10,
-      y: 20
-    },
-    "2": {
-      id: "2",
-      x: 100,
-      y: 200
-    }
-  };
-  private itemList: ItemList = ["1", "2"];
-  private chosenItem: Item | undefined = undefined;
-  private mouseDownCoord: Coord | undefined = undefined;
-
-  public createItem() {
-    const newItem: Item = {
-      id: this.getId(),
-      x: Math.random() * 300,
-      y: Math.random() * 300
-    };
-    this.items[newItem.id] = newItem;
-    this.itemList.push(newItem.id);
-    this.blocks[this.blockList[0]].items.push(newItem.id);
-
-    this.$emit("item-created", newItem);
-  }
-
-  public itemStyle(item: Item) {
-    return {
-      left: item.x + "px",
-      top: item.y + "px"
-    };
-  }
-
-  public mouseDownItem(e: MouseEvent) {
-    const target = e.target as HTMLDivElement;
-    const itemId = target && target.id.replace("item-", "");
-    this.mouseDownCoord = {
-      x: e.pageX,
-      y: e.pageY
-    };
-    this.chosenItem = Object.assign({}, this.items[itemId]);
-    document.addEventListener("mousemove", this.mouseMoveItem);
-    document.addEventListener("mouseup", this.mouseUpItem);
-  }
-
-  private getId(): string {
-    return Math.round(Math.random() * 1000000000).toString();
-  }
-
-  private mouseMoveItem(e: MouseEvent) {
-    if (this.mouseDownCoord && this.chosenItem) {
-      const moveCoordinate = {
-        x: e.pageX - this.mouseDownCoord.x,
-        y: e.pageY - this.mouseDownCoord.y
-      };
-      const id = this.chosenItem.id;
-      if (id) {
-        this.items[id].x = this.chosenItem.x + moveCoordinate.x;
-        this.items[id].y = this.chosenItem.y + moveCoordinate.y;
-      }
-    }
-  }
-
-  private mouseUpItem(e: MouseEvent) {
-    this.mouseDownCoord = undefined;
-    this.chosenItem = undefined;
-    document.removeEventListener("mousemove", this.mouseMoveItem);
-    document.removeEventListener("mouseup", this.mouseUpItem);
-  }
+interface Data {
+  blocks: Blocks;
+  blockList: BlockList;
+  items: Items;
+  itemList: ItemList;
 }
+
+function getId(): string {
+  return Math.round(Math.random() * 1000000000).toString();
+}
+
+export default defineComponent({
+  setup(_, { emit }) {
+    const data = reactive<Data>({
+      blocks: {
+        "1": {
+          id: "1",
+          items: []
+        }
+      },
+      blockList: ["1"],
+      items: {},
+      itemList: []
+    });
+
+    const itemStyle = (item: Item) => {
+      return {
+        left: item.x + "px",
+        top: item.y + "px"
+      };
+    };
+
+    const createItem = () => {
+      const newItem: Item = {
+        id: getId(),
+        x: Math.random() * 300,
+        y: Math.random() * 300
+      };
+      data.items[newItem.id] = newItem;
+      data.itemList.push(newItem.id);
+      data.blocks[data.blockList[0]].items.push(newItem.id);
+
+      emit("item-created", newItem);
+    };
+
+    const mouseDownItem = (e: MouseEvent) => {
+      const target = e.target as HTMLDivElement;
+      const itemId = target && target.id.replace("item-", "");
+      const mouseDownCoord: Coord = {
+        x: e.pageX,
+        y: e.pageY
+      };
+      const chosenItem: Item = Object.assign({}, data.items[itemId]);
+
+      const mouseMoveItem = (e: MouseEvent) => {
+        if (mouseDownCoord && chosenItem) {
+          const moveCoordinate = {
+            x: e.pageX - mouseDownCoord.x,
+            y: e.pageY - mouseDownCoord.y
+          };
+          const id = chosenItem.id;
+          if (id) {
+            data.items[id].x = chosenItem.x + moveCoordinate.x;
+            data.items[id].y = chosenItem.y + moveCoordinate.y;
+          }
+        }
+      };
+
+      const mouseUpItem = (e: MouseEvent) => {
+        document.removeEventListener("mousemove", mouseMoveItem);
+        document.removeEventListener("mouseup", mouseUpItem);
+      };
+
+      document.addEventListener("mousemove", mouseMoveItem);
+      document.addEventListener("mouseup", mouseUpItem);
+    };
+
+    return {
+      ...toRefs(data),
+      createItem,
+      itemStyle,
+      mouseDownItem
+    };
+  }
+});
