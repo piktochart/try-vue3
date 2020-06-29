e044230c6f102b6f963e0baa3287d6766fc721b2
Albert Mulia Shintra <chenxeed@gmail.com>
2020-06-29 16:07:40 +0800

    style: use next layout

--- src/views/Editor/index.vue
+++ src/views/Editor/index.vue
@@ -1,28 +1,46 @@
 <template>
-  <div>
-    <Teleport to="#teleport-target">
-      <h1>Editor</h1>
-    </Teleport>
-    <div>
-      <h1>Toolbar</h1>
-      <div><button @click="onClickCreateImage">Add image</button></div>
-      <div><button @click="onClickCreateText">Add text</button></div>
-      <div>
-        <button :disabled="!refHistory.canUndo" @click="onClickUndo">
-          Undo
-        </button>
-        <button :disabled="!refHistory.canRedo" @click="onClickRedo">
-          Redo
-        </button>
+  <div class="container-full">
+    <div class="d-flex">
+      <div class="toolbar-menu p-2">
+        Toolbar Icons
+      </div>
+      <div class="toolbar-content p-2 border-left">
+        Toolbar
+        <div><button @click="onClickCreateImage">Add image</button></div>
+        <div><button @click="onClickCreateText">Add text</button></div>
+      </div>
+      <div class="flex-fill border-left">
+        <div class="row p-3">
+          <div class="col-3">Visual Name</div>
+          <div class="col-5 text-center">
+            <button :disabled="!refHistory.canUndo" @click="onClickUndo">
+              Undo
+            </button>
+            <button :disabled="!refHistory.canRedo" @click="onClickRedo">
+              Redo
+            </button>
+          </div>
+          <div class="col-4 text-right">Publish Tools</div>
+        </div>
+        <div class="bg-light p-5">
+          <canvas-editor
+            ref="canvasEditor"
+            @mousedown-item="onMouseDownItem"
+            @moving-item="onMovingItem"
+            @moved-item="onMovedItem"
+          />
+        </div>
       </div>
     </div>
-    {{ selectedIds }}
-    <canvas-editor
-      ref="canvasEditor"
-      @mousedown-item="onMouseDownItem"
-      @moving-item="onMovingItem"
-      @moved-item="onMovedItem"
-    />
   </div>
 </template>
 <script lang="ts" src="./"></script>
+<style scoped>
+.toolbar-menu {
+  width: 72px;
+}
+
+.toolbar-content {
+  width: 280px;
+}
+</style>
